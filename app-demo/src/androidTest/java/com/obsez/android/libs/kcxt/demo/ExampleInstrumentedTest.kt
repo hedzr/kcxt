33153352d9fc7a16a6a8ec053f31773e136c798d
Hedzr Yeh <hedzrz@gmail.com>
2019-06-14 22:57:07 +0800

    fighting

--- app-demo/src/androidTest/java/com/obsez/android/libs/kcxt/demo/ExampleInstrumentedTest.kt
+++ app-demo/src/androidTest/java/com/obsez/android/libs/kcxt/demo/ExampleInstrumentedTest.kt
@@ -1,22 +1,22 @@
 package com.obsez.android.libs.kcxt.demo
 
-//import androidx.test.InstrumentationRegistry
-//import androidx.test.runner.AndroidJUnit4
-//import org.junit.Assert.assertEquals
-//import org.junit.Test
-//import org.junit.runner.RunWith
-//
-///**
-// * Instrumented test, which will execute on an Android device.
-// *
-// * See [testing documentation](http://d.android.com/tools/testing).
-// */
-//@RunWith(AndroidJUnit4::class)
-//class ExampleInstrumentedTest {
-//    @Test
-//    fun useAppContext() {
-//        // Context of the app under test.
-//        val appContext = InstrumentationRegistry.getTargetContext()
-//        assertEquals("com.obsez.android.libs.kcxt.demo", appContext.packageName)
-//    }
-//}
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.obsez.android.libs.kcxt.demo", appContext.packageName)
+    }
+}
